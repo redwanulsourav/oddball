3888370f056d80f0a4ab6d9bd69ca3eb6397ff12
redwanulsourav <redwanul.sourav@gmail.com>
2022-06-20 22:56:32 -0400

    Position working

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from __future__ import print_function
 import cv2
+import numpy as np
 from screeninfo import get_monitors
 import random
-from __future__ import print_function
 import PILasOPENCV as Image
 import PILasOPENCV as ImageDraw
 import PILasOPENCV as ImageFont
+
+screenWidth = None
+screenHeight = None
+numberBoxWidth = None
+numberBoxHeight = None
 
 def init():
     global screenWidth
@@ -14,25 +20,35 @@ def init():
 
     for m in get_monitors():
         if m.is_primary == True:
-            screenHeight = m.height
-            screenwidth = m.width
+            screenHeight = m.height -100
+            screenWidth = m.width - 100
+            print("dhukse")
+    print(screenHeight)
+    print(screenWidth)
+    numberBoxWidth = 180
+    numberBoxHeight = 60
 
 def getNumbers():
     return [1, 333, 2, 99, 100, 4]
 
 
 def drawNumbers():
-    img = np.zeros(screenWidth, screenHeight)
-    img = 1 - img
+    global screenWidth
+    global screenHeight
+    global numberBoxWidth
+    global numberBoxHeight
 
-    horizontalBoxCount = screenWidth / numberBoxWidth
-    verticalBoxCount = screenHeight / numberBoxHeight
+    img = np.zeros((screenHeight, screenWidth, 3), dtype=np.uint8)
+    img = 255 - img
+
+    horizontalBoxCount = screenWidth // numberBoxWidth
+    verticalBoxCount = screenHeight // numberBoxHeight
 
     totalBoxCount = horizontalBoxCount * verticalBoxCount
 
     validIndices = []
 
-    for i in ranges(totalBoxCount):
+    for i in range(totalBoxCount):
         validIndices.append(i)
     
     testNumbers = getNumbers()
@@ -48,17 +64,30 @@ def drawNumbers():
         gridColumn = idx % horizontalBoxCount
 
 
-        upperLeftCorner = (gridRow * numberBoxHeight + 20, gridColumn * numberBoxWidth + 20)
+        upperLeftCorner = (gridRow * numberBoxHeight , gridColumn * numberBoxWidth )
         lowerRightCorner = (upperLeftCorner[0] + numberBoxHeight - 20, upperLeftCorner[1] + numberBoxWidth - 20)
 
         boundingBoxes[i] = (upperLeftCorner, lowerRightCorner)
-    
-    font = ImageFont.truetype("arial.ttf", 24)
-    pilImage = Image.fromarray(np.uint8(numpy_image))
+        assert(upperLeftCorner[0] >= 0 and upperLeftCorner[0] < screenHeight and upperLeftCorner[1] >= 0 and upperLeftCorner[1] < screenWidth)
+    font = ImageFont.truetype("arial.ttf", 28)
+    pilImage = Image.new('RGB', (screenWidth, screenHeight  ), 'black')
+    pilImage.setim(img)
 
-    for key,value in boundingBoxes:
+    draw = ImageDraw.Draw(pilImage)
+    print(boundingBoxes)
+    
+    for key,value in boundingBoxes.items():
         text = str(key)
-        draw.text((value[0][1], value[0][0]), text, font=font, fill=(0, 0, 0))
+        draw.text((value[0][1], value[0][0]), text, font=font, fill=(1,1,1))
     
+    mask = pilImage.getim()
+    cv2.imshow('mask', mask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+def main():
+    drawNumbers()
 
+if __name__ == '__main__':
+    init()
+    main()
