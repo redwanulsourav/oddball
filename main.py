e394d63a7104b614d4cd0b3ec5ef79bb003b8e03
redwanulsourav <redwanul.sourav@gmail.com>
2022-06-20 23:23:25 -0400

    Click event working now

--- main.py
+++ main.py
@@ -6,11 +6,50 @@ import random
 import PILasOPENCV as Image
 import PILasOPENCV as ImageDraw
 import PILasOPENCV as ImageFont
+import datetime
 
 screenWidth = None
 screenHeight = None
 numberBoxWidth = None
 numberBoxHeight = None
+boundingBoxes = None
+testNumbers = None
+
+def writeFile(num):
+    print("called 2")
+    now = datetime.datetime.now()
+    f = open(str(now) + '.txt', 'w')
+    result = ''
+    init = True
+    for x in testNumbers:
+        if init == False:
+            result += ','
+        
+        result = result + str(x)
+        init = False
+    
+    result = result + '\n' + str(num)
+    f.write(result)
+    f.close()
+
+def handleClick(event,x,y,flags,param):
+    global boundingBoxes
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        # print("called", x, y)
+        
+        for key, value in boundingBoxes.items():
+            x0 = value[0][1]
+            y0 = value[0][0]
+
+            x1 = value[1][1]
+            y1 = value[1][0]
+
+            if x >= x0 and y >= y0 and x <= x1 and y <= y1:
+                writeFile(key)
+                # return;
+        cv2.destroyAllWindows()
+        exit()
+
 
 def init():
     global screenWidth
@@ -20,8 +59,8 @@ def init():
 
     for m in get_monitors():
         if m.is_primary == True:
-            screenHeight = m.height -100
-            screenWidth = m.width - 100
+            screenHeight = m.height -200
+            screenWidth = m.width - 200
             print("dhukse")
     print(screenHeight)
     print(screenWidth)
@@ -37,6 +76,8 @@ def drawNumbers():
     global screenHeight
     global numberBoxWidth
     global numberBoxHeight
+    global boundingBoxes
+    global testNumbers
 
     img = np.zeros((screenHeight, screenWidth, 3), dtype=np.uint8)
     img = 255 - img
@@ -58,7 +99,7 @@ def drawNumbers():
     }
 
     for i in testNumbers: 
-        idx = validIndices[random.randint(0, len(validIndices))]
+        idx = validIndices[random.randint(0, len(validIndices)-1)]
         
         gridRow = idx / horizontalBoxCount
         gridColumn = idx % horizontalBoxCount
@@ -81,9 +122,11 @@ def drawNumbers():
         draw.text((value[0][1], value[0][0]), text, font=font, fill=(1,1,1))
     
     mask = pilImage.getim()
-    cv2.imshow('mask', mask)
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', handleClick)
+    cv2.imshow('image', mask)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
 def main():
     drawNumbers()
