fe1176c5f209e30f3c581d49b8eaf4dc140f938c
redwanulsourav <redwanul.sourav@gmail.com>
2022-06-21 00:06:22 -0400

    Seems to be done

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import PILasOPENCV as Image
 import PILasOPENCV as ImageDraw
 import PILasOPENCV as ImageFont
 import datetime
+from scipy.stats import truncnorm
 
 screenWidth = None
 screenHeight = None
@@ -68,7 +69,55 @@ def init():
     numberBoxHeight = 60
 
 def getNumbers():
-    return [1, 333, 2, 99, 100, 4]
+    digitCount = random.randint(1, 3)
+    if digitCount == 1:
+        low = 0
+        high = 9
+    elif digitCount == 2:
+        low = 10
+        high = 99
+    else:
+        low = 100
+        high = 999
+
+    
+    mu, sigma = 0.5, 1
+    lower, upper = mu - 0.1*sigma, mu + 0.1*sigma
+
+    sizeList = [3, 5, 7]
+    size = sizeList[random.randint(1,3)-1]
+
+    samples = truncnorm.rvs(
+          (lower-mu)/sigma,(upper-mu)/sigma,loc=mu,scale=sigma,size=size)
+    
+    lower, upper = -10000000, mu  -2.698*sigma
+    outlier1 = truncnorm.rvs(
+          (lower-mu)/sigma,(upper-mu)/sigma,loc=mu,scale=sigma,size=1)
+
+    lower, upper = mu  + 2.698*sigma, 10000000
+    outlier2 = truncnorm.rvs(
+          (lower-mu)/sigma,(upper-mu)/sigma,loc=mu,scale=sigma,size=1)
+    
+    outlier = None
+    if random.randint(1, 2) == 1:
+        outlier = outlier1
+    else:
+        outlier = outlier2
+
+    outlier = outlier * (high-low) + low
+    outlier = int(np.ceil(outlier).item())
+
+    samples = (samples * (high-low)) + low
+    
+    print(type(samples))   
+    
+    
+    samples = np.ceil(samples)
+    samples = samples.astype(np.uint16)
+
+    samples = list(samples)
+    samples.append(outlier)
+    return samples
 
 
 def drawNumbers():
